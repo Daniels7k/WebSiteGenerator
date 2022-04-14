0aaded3d4c25f96bbbbe7e0724cf7d516899f740
Cleiton Daniel (Kaonami) <danielsdev77@gmail.com>
2022-04-14 09:15:22 -0300

    Pequenos ajustes

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -6,4 +6,8 @@ const UserController = require("../controllers/userController")
 router.get("/registro", UserController.registroGet)
 router.post("/registroPost", UserController.registroPost )
 
+//MeuSite
+router.get("/meusite", (req, res) => {
+    res.render("usuario/meusite")
+})
 module.exports = router
